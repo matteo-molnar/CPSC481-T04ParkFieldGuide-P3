cced9cb44168f04afa42bb22c0e90f485ee9010d
Matteo Molnar <matteo.molnar@gmail.com>
2016-11-24 13:27:09 -0700

    WildLife Menu Implemented With Moose Information
    Removed the filler text to test the scrolling for the wildlife menu and
    created a listing for the moose and info that comes up when you tap the
    name. NOTE: not sure why scan is crashing when some buttons are pushed
    and such, need to figure that out :/

---
+++ ParkFieldGuideHi-FiProtoType/app/src/main/java/com/p3/t04parkfieldguide/parkfieldguidehi_fiprototype/MooseInfo.java
@@ -0,0 +1,13 @@
+package com.p3.t04parkfieldguide.parkfieldguidehi_fiprototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MooseInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moose_info);
+    }
+}
